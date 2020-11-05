2aa4da8f0ec5616e52b8e625f6c49236bc9133c2
quety <tianya3232@126.com>
2020-11-05 10:11:15 +0800

    add

--- app/Http/Controllers/AuthenticationController.php
+++ app/Http/Controllers/AuthenticationController.php
@@ -4,7 +4,39 @@
 namespace App\Http\Controllers;
 
 
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Laravel\Socialite\Facades\Socialite;
+
 class AuthenticationController extends Controller
 {
+    public function getSocialRedirect($account)
+    {
+        try {
+            return Socialite::with($account)->redirect();
+        } catch (\InvalidArgumentException $exception) {
+            return redirect("login");
+        }
+    }
 
+    public function getSocialCallback($account, User $newUser)
+    {
+        $socialUser = Socialite::with($account)->user();
+        $user = User::where("provider_id", $socialUser->id)
+            ->where("provider", $account)
+            ->first();
+        if (null == $user) {
+            $newUser->name        = $socialUser->getName();
+            $newUser->email       = $socialUser->getEmail() == '' ? '' : $socialUser->getEmail();
+            $newUser->avatar      = $socialUser->getAvatar();
+            $newUser->password    = '';
+            $newUser->provider    = $account;
+            $newUser->provider_id = $socialUser->getId();
+            $newUser->save();
+            $user = $newUser;
+        }
+        Auth::login($user);
+
+        return redirect("/");
+    }
 }
